6fd64ac2a2fda32d8f6180b8fa974e1ae72b9099
Miguel Savignano <migue.masx@gmail.com>
2017-06-19 19:56:03 +0200

    Update mongoid_filters.rb
    add custom filtering, example "__regexp", build a regexp ignoring accents

--- lib/mongoid_filters.rb
+++ lib/mongoid_filters.rb
@@ -1,37 +1,50 @@
 module QueryToFilterQuery
   extend self
-    # { "amount.$gte": 100 }
+    # { "amount__gte": 100 }
     # to
     # { :amount.gte => 100 }
 
-    # { age: 28, name: "" }
+    # { "amount": 100, "name": "" }
     # to
-    # { age: 28 }
+    # { amount: 100 }
+
+    # { "categories_ids": [""], amount: 28 }
+    # to
+    # { amount: 100 }
 
   def run(query = {})
+    query ||= {}
     filter_query = {}
     query.each do |key, value|
-      new_key = to_mongoid_criteria_queryable_key(key)
-      filter_query[new_key] = value
+      new_key, new_value = to_mongoid_criteria_queryable(key, value)
+      filter_query[new_key] = new_value
     end
-    filter_query = filter_query.select{ |key,value| value != "" }
+    filter_query = filter_query.select{ |key, value| !empty_value?(value) }
   end
 
-  def to_mongoid_criteria_queryable_key(hash_key = "")
-    if custom_filter?(hash_key)
+  def to_mongoid_criteria_queryable(hash_key = "", value)
+    if custom_filter?(hash_key, mongoid_filter_operators)
       name, operator = hash_key.to_s.split(operator_prefix)
-      name.to_sym.send(operator)
+      [name.to_sym.send(operator), value]
+    elsif custom_filter?(hash_key, custom_filter_operators)
+      name, operator = hash_key.to_s.split(operator_prefix)
+      set_custom_filters(name, value, operator)
     else
-      hash_key
+      [hash_key, value]
     end
   end
 
-  def custom_filter?(hash_key)
+  def custom_filter?(hash_key, opterators)
     if hash_key.is_a?(String) || hash_key.is_a?(Symbol)
-      mongoid_filter_operators.any?{ |operator| hash_key.to_s.include?("#{operator_prefix}#{operator}") }
+      opterators.any?{ |operator| hash_key.to_s.include?("#{operator_prefix}#{operator}") }
     else
       false
     end
+  end
+
+  def set_custom_filters(key, value, operator)
+    value = /#{build_regexp_accents(value)}/i if operator == "regexp" && !empty_value?(value)
+    [key, value]
   end
 
   def operator_prefix
@@ -39,7 +52,28 @@ module QueryToFilterQuery
   end
 
   def mongoid_filter_operators
-    %W[ gte lte in with_size ]
+    %W[ gte lte in with_size nin ]
+  end
+
+  def custom_filter_operators
+    %W[ regexp ]
+  end
+
+  def build_regexp_accents(q = "")
+    q ||= ""
+    q.gsub(/(a|á)/i, "(a|á)")
+     .gsub(/(e|é)/i, "(e|é)")
+     .gsub(/(i|í)/i, "(i|í)")
+     .gsub(/(o|ó)/i, "(o|ó)")
+     .gsub(/(u|ú)/i, "(u|ú)")
+  end
+
+  def empty_value?(value)
+    if value.is_a?(Array)
+      value.reject{|v| v == ""}.blank?
+    else
+      value == ""
+    end
   end
 
 end
